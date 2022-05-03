9db279c2d637907ab0b25fb4ecd6d01104b95545
Willian Figueiredo <williamdev38@gmail.com>
2022-05-03 15:24:30 -0300

    Correção do nome da class

--- bankline-api/src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ bankline-api/src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -2,7 +2,7 @@ package com.dio.santander.bankline.api.model;
 
 import java.time.LocalDateTime;
 
-public class Mvimentacao {
+public class Movimentacao {
 	
 	private Integer id;
 	private LocalDateTime dataHora;
